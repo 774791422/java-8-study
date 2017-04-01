60e18e88eabd9fabdbcabee4fe8b8ae754989e19
774791422 <774791422@qq.com>
2017-04-01 17:40:35 +0800

    Stream 练习

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -4,15 +4,17 @@ import java.util.function.Supplier;
  * Created by qiuzhanghua on 2017/2/7.
  */
 public class FibonacciSupplier implements Supplier<Long> {
-  private long previous = 1;
-  private long current = 2;
+  private long one = 1;
+  private long two = 2;
+  private long three = 4;
 
   @Override
   public Long get() {
-    long p = previous;
-    long next = current + previous;
-    previous = current;
-    current = next;
+    long p = one;
+    long next = three+two+one;
+    one = two;
+    two = three;
+    three = next;
     return p;
   }
 }
