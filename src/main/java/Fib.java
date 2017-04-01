be0b3d9d9cb2c288e7bc38106b4abfe22c110cc6
774791422 <774791422@qq.com>
2017-04-01 17:47:35 +0800

    123

--- src/main/java/Fib.java
+++ src/main/java/Fib.java
@@ -6,6 +6,6 @@ import java.util.stream.Stream;
 public class Fib { 
   public static void main(String[] args) {
     Stream<Long> longStream = Stream.generate(new FibonacciSupplier());
-    longStream.limit(10).forEach(System.out::println);
+    longStream.limit(10).forEach(System.out::println); 
   }
 }
